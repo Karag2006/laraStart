b4ddbaab270470c01dd57af62a9e2582b3c28df2
Martin Richter <karag2006@gmx.net>
2020-03-21 12:07:11 +0100

    Lesson 10

--- resources/js/components/Users.vue
+++ resources/js/components/Users.vue
@@ -1,16 +1,69 @@
 <template>
     <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Users Component</div>
+        <div class="row mt-5">
+        <div class="col-md-12">
+          <div class="card">
+            <div class="card-header">
+              <h3 class="card-title">User Management</h3>
 
-                    <div class="card-body">
-                        I'm an example component.
-                    </div>
-                </div>
+              <div class="card-tools">
+                <button class="btn btn-success" data-toggle="modal" data-target="#addNew">Add User <i class="fas fa-user-plus fa-fw"></i> </button>
+              </div>
             </div>
+            <!-- /.card-header -->
+            <div class="card-body table-responsive no-padding">
+              <table class="table table-hover">
+                <tbody><tr>
+                  <th>ID</th>
+                  <th>Name</th>
+                  <th>E-Mail</th>
+                  <th>Type</th>
+                  <th>Modify</th>
+                </tr>
+                <tr>
+                  <td>183</td>
+                  <td>John Doe</td>
+                  <td>11-7-2014</td>
+                  <td><span class="label label-success">Approved</span></td>
+                  <td>
+                    <a href="#">
+                        <i class="fas fa-edit blue"></i>
+                    </a>
+                     /
+                    <a href="#">
+                        <i class="fas fa-minus-circle red"></i>
+                    </a>
+                  </td>
+                </tr>
+              </tbody></table>
+            </div>
+            <!-- /.box-body -->
+          </div>
+          <!-- /.box -->
         </div>
+      </div>
+
+    <!-- Modal -->
+<div class="modal fade" id="addNew" tabindex="-1" role="dialog" aria-labelledby="addNewLabel" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="addNewLabel">Add New User</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-success">Create</button>
+      </div>
+    </div>
+  </div>
+</div>
+
     </div>
 </template>
 
