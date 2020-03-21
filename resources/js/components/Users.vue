9e63469622bd1f1dd1e135eb43e2df0bdc2984a4
Martin Richter <karag2006@gmx.net>
2020-03-21 21:50:21 +0100

    Lesson 13

--- resources/js/components/Users.vue
+++ resources/js/components/Users.vue
@@ -95,7 +95,7 @@
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-success">Create</button>
+        <button type="submit" class="btn btn-success">Create</button>
       </div>
       </form>
     </div>
